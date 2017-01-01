6f353b72d998b0ea61312f651160101befd88fb3
Kenta Watanabe <soukaijapan@gmail.com>
2017-01-01 07:07:04 +0000

    adding_indents

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,33 +1,33 @@
 class ContactsController < ApplicationController
+  
   def index
   end
    
   def new
-   if params[:back]
+    if params[:back]
+      @contact = Contact.new(contacts_params)
+     else
+      @contact = Contact.new
+    end 
+  end
+  def create
     @contact = Contact.new(contacts_params)
-   else
-    @contact = Contact.new
-   end 
+    if @contact.save
+      redirect_to root_path, notice: "お問い合わせが完了しました！"
+    else
+      render 'new'
+    end
   end
  
- def create
-  @contact = Contact.new(contacts_params)
-   if @contact.save
-    redirect_to root_path, notice: "お問い合わせが完了しました！"
-   else
-     render 'new'
-   end
- end
- 
  def confirm
-  @contact = Contact.new(contacts_params)
-  render :new if @contact.invalid?
+    @contact = Contact.new(contacts_params)
+    render :new if @contact.invalid?
  end
   
   
  private
   def contacts_params
-   params.require(:contact).permit(:name, :email, :content)
+    params.require(:contact).permit(:name, :email, :content)
   end
 
 end
