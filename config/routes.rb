2b50dab36347bd0e0a02913700e84db3f8a5c4fc
Kenta Watanabe <soukaijapan@gmail.com>
2017-01-23 03:17:09 +0000

    add_ActionMailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'top#index'
   devise_for :users
-  
+
   resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy] do
     collection do
       post :confirm
@@ -14,7 +14,11 @@ Rails.application.routes.draw do
     end
   end
 
-  
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at:"/letter_opener"
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
